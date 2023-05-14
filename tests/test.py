95833c767c5082f6e433f12930019e126ed86278
benx1n <1119809439@qq.com>
2023-05-14 23:14:50 +0800

    [🚀]update 0.1.4

--- tests/test.py
+++ tests/test.py
@@ -4,10 +4,9 @@ from hikari_core import init_hikari, set_hikari_config
 
 
 async def start():
-    # browser = await get_browser(use_browser="chronium")
-    # await set_config(auto_rendering=False, use_browser="chronium")
     set_hikari_config(use_broswer="firefox", http2=False, proxy="http://localhost:7890")
     hikari_data = await init_hikari("QQ", "1119809439", "me")
-    return
+
+
 if __name__ == '__main__':
     asyncio.run(start())
