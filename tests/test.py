486efd677a4523c88811594863d12c29c5e39731
benx1n <1119809439@qq.com>
2023-05-15 23:06:27 +0800

    [#]更新测试

--- tests/test.py
+++ tests/test.py
@@ -5,15 +5,16 @@ from pathlib import Path
 dir_path = Path(__file__).parent.parent
 sys.path.append(f"{dir_path}")
 
-from hikari_core import init_hikari, set_hikari_config
+from hikari_core import hikari_config, init_hikari, set_hikari_config
 
 
 async def start():
     set_hikari_config(use_broswer="chromium", http2=False, proxy="http://localhost:7890")
     hikari_data = await init_hikari("QQ", "1119809439", "me")
-    with open('test.png', 'wb') as f:
-        f.write(hikari_data.Output.Data)
-    await asyncio.sleep(100)
+    if hikari_config.auto_rendering and hikari_config.auto_image:
+        with open('test.png', 'wb') as f:
+            f.write(hikari_data.Output.Data)
+    await asyncio.sleep(5)
 
 
 if __name__ == '__main__':
