99e7733f33a40758dbf5c102354db35ed99132a9
benx1n <1119809439@qq.com>
2023-09-14 15:16:06 +0800

    [#]临时适配新版扫雪

--- tests/test.py
+++ tests/test.py
@@ -13,7 +13,7 @@ async def start():
     global start_time
     start_time = time.time()
     set_hikari_config(use_broswer='chromium', http2=False, proxy='http://localhost:7890', token='test:yuyuko_test')
-    hikari_data = await init_hikari('QQ', '1119809439', 'me recent', '693433753', Ignore_List=[get_ClanInfo, get_AccountInfo])
+    hikari_data = await init_hikari('QQ', '1119809439', 'me sx', '693433753', Ignore_List=[get_ClanInfo, get_AccountInfo])
     if hikari_data.Status == 'success':
         await output_with_check_type(hikari_data)
     elif hikari_data.Status == 'wait':
