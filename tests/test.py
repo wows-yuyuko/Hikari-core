7db9cbeeee9c5a0c8781e59551512470d141170a
benx1n <1119809439@qq.com>
2023-09-28 02:08:26 +0800

    [#]更新recents模板

--- tests/test.py
+++ tests/test.py
@@ -13,7 +13,7 @@ async def start():
     global start_time
     start_time = time.time()
     set_hikari_config(use_broswer='chromium', http2=False, proxy='http://localhost:7890', token='test:yuyuko_test', yuyuko_type='CHANNEL')
-    hikari_data = await init_hikari('QQ', '2257844796', 'me ship 里希特', '693433753', Ignore_List=[get_ClanInfo])
+    hikari_data = await init_hikari('QQ', '2765157865', 'me recents 2', '693433753', Ignore_List=[get_ClanInfo])
     if hikari_data.Status == 'success':
         await output_with_check_type(hikari_data)
     elif hikari_data.Status == 'wait':
